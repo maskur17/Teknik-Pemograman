7b3507d2fb61475142a39684ef2af8be131f7139
maskur17 <75474054+maskur17@users.noreply.github.com>
2021-04-18 22:10:05 +0700

    Add files via upload

---
+++ FirstSample/FirstSample.java
@@ -0,0 +1,15 @@
+package FirstSample;
+
+/**
+ * This is teh first sample program in Core Java Chapter 3
+ * @version 
+ * @author Gary Cornell
+ */
+
+    public class FirstSample {
+
+        public static void main (String[] args) {
+
+            System.out.println("We will not use 'Hello, World!'");
+        }
+    }
