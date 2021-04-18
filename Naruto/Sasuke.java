29d18e2439cd84cf9198296d0909b5b10d9776e7
maskur17 <75474054+maskur17@users.noreply.github.com>
2021-04-18 21:41:07 +0700

    Add files via upload

---
+++ Naruto/Sasuke.java
@@ -0,0 +1,12 @@
+package Naruto;
+
+public class Sasuke extends Itachi {
+	String Dojutsu = "Sharingan";
+
+	public void printDojutsu() {
+		super.printDojutsu();
+		System.out.println(this.Dojutsu);
+
+
+	}
+}
